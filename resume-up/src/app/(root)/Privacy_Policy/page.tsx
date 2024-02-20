aee20476502d980546a5a8b33cec4a0510e4d17f
Nick Thomas <nick02@iastate.edu>
2024-02-19 18:11:26 -0600

    Privacy Policy text added

--- resume-up/src/app/(root)/Privacy_Policy/page.tsx
+++ resume-up/src/app/(root)/Privacy_Policy/page.tsx
@@ -1,5 +1,30 @@
 const page = () => {
-  return <div>page</div>;
+  return <div>
+    <h1>Privacy Policy</h1>
+    <p>Last Updated: 2/19/24</p>
+    <p>This page informs you of our policies regarding the collection, use, and disclosure of personal data when you
+        use our Service and the choices you have associated with that data.</p>
+    <p>We use your data to provide and improve the Service. By using the Service, you agree to the collection and use
+        of information in accordance with this policy.</p>
+
+    <h2>Information Collection and Use</h2>
+    <p>We collect several different types of information for various purposes to provide and improve our Service to
+        you.</p>
+
+    <h3>Types of Data Collected</h3>
+
+    <h4>Personal Data</h4>
+    <p>While using our Service, we may ask you to provide us with certain personally identifiable information that can
+        be used to contact or identify you (Personal Data). Personally identifiable information may include, but is
+        not limited to:</p>
+    <ul>
+        <li>Email address</li>
+        <li>First name and last name</li>
+        <li>Phone number</li>
+        <li>Address, State, Province, ZIP/Postal code, City</li>
+        <li>Cookies and Usage Data</li>
+    </ul>
+  </div>;
 };
 
 export default page;

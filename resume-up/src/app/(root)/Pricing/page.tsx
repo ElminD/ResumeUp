7f619e9ddd41cbf21cb65f0c86b7c6ec3b011d20
njboldt <njboldt@iastate.edu>
2024-04-23 14:59:54 -0500

    Updated pricing page to look more like figma - needs other image

--- resume-up/src/app/(root)/Pricing/page.tsx
+++ resume-up/src/app/(root)/Pricing/page.tsx
@@ -1,39 +1,27 @@
+import { Button } from "@/components/ui/button"
+
 const page = () => {
-    return <div className="max-w-6xl mx-auto px-4 py-10">
-      <h1 className="text-4xl font-bold mb-6 ml-3 text-resumeup">Privacy Policy</h1>
-      <p className="text-lg mb-6 ml-3">Last Updated: 2/19/24</p>
-      <p className="text-lg ml-3">This page informs you of our policies regarding the collection, use, and disclosure of personal data when you
-          use our Service and the choices you have associated with that data.</p>
-      <p className="text-lg ml-3">We use your data to provide and improve the Service. By using the Service, you agree to the collection and use
-          of information in accordance with this policy.</p>
-  
-      <h2 className="text-2xl font-bold ml-3">Information Collection and Use</h2>
-      <p className="text-lg ml-3">We collect several different types of information for various purposes to provide and improve our Service to
-          you.</p>
-  
-      <h3 className="text-2xl font-bold ml-3">Types of Data Collected</h3>
-  
-      <h4 className="text-lg ml-3">Personal Data</h4>
-      <p className="text-lg ml-3">While using our Service, we may ask you to provide us with certain personally identifiable information that can
-          be used to contact or identify you (Personal Data). Personally identifiable information may include, but is
-          not limited to:</p>
-      <ul>
-          <li className="text-lg ml-3">&nbsp; •Email address</li>
-          <li className="text-lg ml-3">&nbsp; •First name and last name</li>
-          <li className="text-lg ml-3">&nbsp; •Phone number</li>
-          <li className="text-lg ml-3">&nbsp; •Address, State, Province, ZIP/Postal code, City</li>
-          <li className="text-lg ml-3">&nbsp; •Cookies and Usage Data</li>
-      </ul>
-  
-      <h3 className="text-2xl font-bold ml-3">Data Security</h3>
-      <p className="text-lg ml-3">We prioritize the security of your personal information and employ industry-standard measures to protect it from unauthorized access, disclosure, alteration, or destruction. However, please note that no method of transmission over the internet or electronic storage is completely secure, and we cannot guarantee absolute security.</p>
-      <p className="text-lg ml-3">In the event of a data breach that affects your personal data, we will notify you and relevant authorities as required by applicable laws and regulations.</p>
-  
-      <h3 className="text-2xl font-bold ml-3">Third-Party Links</h3>
-      <p className="text-lg ml-3">Our Service may contain links to third-party websites or services that are not operated by us. We have no control over, and assume no responsibility for, the content, privacy policies, or practices of any third-party websites or services. We advise you to review the privacy policies of every website you visit.</p>
-      <p className="text-lg ml-3">Additionally, we may use third-party services to assist us in analyzing how our Service is used. These services may collect information about your use of our Service but do not access, use, or share your personal information for any other purpose.</p>
-  
-    </div>;
+    return (
+    <div className="flex justify-between max-w-6xl mx-auto px-4 py-10" >
+        <div className="ourStory flex-1 mr-20">
+          <h1 className="text-4xl font-bold mb-6 text-resumeup">Invest in Your Future</h1>
+          <p className="text-lg mb-6">
+          Upgrade to our Pro Plan on ResumeUp, and unlock a wealth of resources that elevate your resume creation experience. 
+          With an expanded array of professionally crafted templates, our Pro Plan provides unparalleled options, allowing you 
+          to tailor your resume to your unique style and career aspirations. Additionally, enjoy an increased number of AI suggestion tokens, 
+          empowering you to harness the full potential of artificial intelligence in refining your resume. Stand out effortlessly with the Pro Plan 
+          on ResumeUp, where innovation and customization converge to propel your professional journey to new heights.
+          </p>
+          <div className="flex justify-center mt-4"><Button className="submit-button hover:bg-[#d3d3d3]" disabled>Coming Soon</Button></div>
+        </div>
+        <div className="homePic flex-1 mr-20">
+          <div className="flex flex-col items-center">
+            <img src="/homepage.png" alt="Elmin" className="w-100 h-100 mb-2" />
+          </div>
+          
+        </div>
+      </div>
+    );
   };
   
   export default page;
